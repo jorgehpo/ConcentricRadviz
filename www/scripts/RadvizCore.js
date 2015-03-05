5b88c90f2df280a9822db0282f3c8b521a097774
Jorge <jorgehpo@gmail.com>
2015-03-05 19:13:42 -0300

    RadvizCore

---
+++ www/scripts/RadvizCore.js
@@ -0,0 +1,75 @@
+//RadvizCore.js
+//Core functions to compute radviz from Json data
+
+ 
+function normalizeData(m){
+  var min = m[0].slice(); //copy array by value
+  var max = m[0].slice(); //copy array by value
+  for (var i = 0; i < m.length; i++){
+    for (var j = 0; j < m[0].length; j++){
+      if (m[i][j] < min[j]){
+        min[j] = m[i][j];
+      }
+      if (m[i][j] > max[j]){
+        max[j] = m[i][j];
+      }
+    }
+  }
+    
+  for (var i = 0; i < m.length; i++){
+    for (var j = 0; j < m[0].length; j++){
+      m[i][j] = (m[i][j] - min[j]) / (max[j] - min[j])
+    }
+  }
+}
+
+
+function selectColumns(data, columns){
+  var mat = [];
+  columns.forEach(function (c){
+    mat.push(data[c])
+  });
+  mat = numeric.transpose(mat)
+  return (mat)
+}
+
+
+function getAnchorPositions(data, dimNames){
+  
+}
+
+
+function radviz(data, tooltip){
+  normalizeData(data) //normalize columns [0,1]
+  var nAnchors = data[0].length;
+  var angle = 2*Math.PI/nAnchors
+  var rotMat = [[Math.cos(angle), -Math.sin(angle)], [Math.sin(angle), Math.cos(angle)]];
+  
+  var anchors = [[0,1]];
+  
+  for (var i = 1; i < nAnchors; i++){
+    var newAnchor = numeric.dot(rotMat, anchors[i-1]);
+    anchors.push(newAnchor);
+  }
+  
+  
+  var nrow = data.length;
+  var ncol = data[0].length;
+  var proj = [];
+  for (var i = 0; i < nrow; i++){
+    var yi = numeric.sum(data[i]);
+    var _x = 0 , _y = 0;
+    for (j = 0; j < ncol; j++){
+      _x = _x + anchors[j][0] * data[i][j];
+      _y = _y + anchors[j][1] * data[i][j];
+    }
+    _x = _x/yi;
+    _y = _y/yi;
+    if (tooltip){
+      proj.push({x:_x, y:_y, tip:tooltip[i]});
+    }else{
+      proj.push({x:_x, y:_y});
+    }
+  }
+  return (proj)
+}//end - function radviz
--- www/scripts/scriptRadviz.js
+++ www/scripts/scriptRadviz.js
@@ -1,74 +1,5 @@
 //scriptRadviz.js
 
-function normalizeData(m){
-  var min = m[0].slice(); //copy array by value
-  var max = m[0].slice(); //copy array by value
-  for (var i = 0; i < m.length; i++){
-    for (var j = 0; j < m[0].length; j++){
-      if (m[i][j] < min[j]){
-        min[j] = m[i][j];
-      }
-      if (m[i][j] > max[j]){
-        max[j] = m[i][j];
-      }
-    }
-  }
-    
-  for (var i = 0; i < m.length; i++){
-    for (var j = 0; j < m[0].length; j++){
-      m[i][j] = (m[i][j] - min[j]) / (max[j] - min[j])
-    }
-  }
-}
-
-
-function selectColumns(data, columns){
-  var mat = [];
-  columns.forEach(function (c){
-    mat.push(data[c])
-  });
-  mat = numeric.transpose(mat)
-  return (mat)
-}
-
-
-
-function radviz(data, tooltip){
-  normalizeData(data) //normalize columns [0,1]
-  var nAnchors = data[0].length;
-  var angle = 2*Math.PI/nAnchors
-  var rotMat = [[Math.cos(angle), -Math.sin(angle)], [Math.sin(angle), Math.cos(angle)]];
-  
-  var anchors = [[0,1]];
-  
-  for (var i = 1; i < nAnchors; i++){
-    var newAnchor = numeric.dot(rotMat, anchors[i-1]);
-    anchors.push(newAnchor);
-  }
-  
-  
-  var nrow = data.length;
-  var ncol = data[0].length;
-  var proj = [];
-  for (var i = 0; i < nrow; i++){
-    var yi = numeric.sum(data[i]);
-    var _x = 0 , _y = 0;
-    for (j = 0; j < ncol; j++){
-      _x = _x + anchors[j][0] * data[i][j];
-      _y = _y + anchors[j][1] * data[i][j];
-    }
-    _x = _x/yi;
-    _y = _y/yi;
-    if (tooltip){
-      proj.push({x:_x, y:_y, tip:tooltip[i]});
-    }else{
-      proj.push({x:_x, y:_y});
-    }
-  }
-  return (proj)
-}//end - function radviz
-
-
 var networdOutputBindingRadviz = new Shiny.OutputBinding();
 $.extend(networdOutputBindingRadviz, {
     find: function(scope) {
@@ -79,7 +10,7 @@ $.extend(networdOutputBindingRadviz, {
         //-----------------------------
         //-Código de Exemplo
         var radViews = new RadvizViews(el,{diameter: 800, circleOffset: 20});
-
+/*
         radViews.addCircle(new RadvizViewsCircle("Group 1","#27ae60",[{name: "A", pos: 0},{name: "B", pos: 90},{name: "C", pos: 180},{name: "D", pos: 270}]));
         radViews.addCircle(new RadvizViewsCircle("Group 2","#16a085",[{name: "E", pos: 45},{name: "F", pos: 135},{name: "G", pos: 225}]));
         radViews.addCircle(new RadvizViewsCircle("Group 3","#2980b9",[{name: "H", pos: 30},{name: "I", pos: 195},{name: "J", pos: 260}]));
@@ -91,17 +22,26 @@ $.extend(networdOutputBindingRadviz, {
             radViews.addCircle(new RadvizViewsCircle("Group 4","#16a085",[{name: "W", pos: 60},{name: "Y", pos: 145},{name: "Z", pos: 230}]));
         },2500);
         
- 
+*/
+
     //%%%%%%%%%%%%%%%%% RADVIZ AND PLOTTING %%%%%%%%%%%%%%%%%%%%%%%
     var columnsGenre = ["genre_tzanetakis.blu", "genre_tzanetakis.cla", "genre_tzanetakis.cou", "genre_tzanetakis.dis", "genre_tzanetakis.hip", "genre_tzanetakis.jaz", "genre_tzanetakis.met", "genre_tzanetakis.pop", "genre_tzanetakis.reg", "genre_tzanetakis.roc"];
     
     var columnsHumor = ["mood_acoustic.acoustic",   "mood_aggressive.aggressive", "mood_electronic.electronic", "mood_happy.happy", "mood_party.party", "mood_relaxed.relaxed", "mood_sad.sad" ];
     
+    
+    
+    
+    
     var mydat = selectColumns(info.data, columnsGenre);
     
     var smallestCircle = radViews.getSmallestCircleRadius();
     
+    
+    getAnchorPositions(info.data, columnsGenre)
+    
     var rad = radviz(mydat, info.tags.filename)
+    
     
     
     var xValue = function(d){return d.x;};
